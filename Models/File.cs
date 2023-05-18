50f4c4e59f4a43fe72c74394e376be6e2ace662b
jesperkeden <83838381+jesperkeden@users.noreply.github.com>
2023-05-18 15:35:12 +0200

    flera updates till css och layout

--- Models/File.cs
+++ Models/File.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DigitalAfterlife2._0.Models
 {
     public class File
     {
         public int Id { get; set; }
+        [Display(Name = "")]
         public string UploadedFile { get; set; }
         public DateTime DateOfUpload { get; set; }
         public virtual Perished? Perished { get; set; }
