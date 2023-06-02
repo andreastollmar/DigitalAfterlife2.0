2e623bd92fd19866099b8d65cd4bc28968b786ed
andreastollmar <andreastollmar@gmail.com>
2023-06-02 11:34:58 +0200

    ta bort ärende

--- Pages/Portal/Ärende.cshtml.cs
+++ Pages/Portal/Ärende.cshtml.cs
@@ -17,7 +17,7 @@ namespace DigitalAfterlife2._0.Pages.Portal
         public List<Perished>? Perished { get; set; }
 
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync(int deleteId)
         {
 
             var check = _context.NextOfKin.Where(x => x.LoginId == User.FindFirstValue(ClaimTypes.NameIdentifier)).FirstOrDefault();
@@ -25,6 +25,17 @@ namespace DigitalAfterlife2._0.Pages.Portal
             if (check != null)
             {
                 Perished = _context.Perished.Where(x => x.NextOfKinId == check.Id).ToList();
+            }
+
+            if(deleteId != 0)
+            {
+                var persihedToDelete = await _context.Perished.FindAsync(deleteId);
+                if(persihedToDelete != null)
+                {
+                    _context.Perished.Remove(persihedToDelete);
+                    await _context.SaveChangesAsync();
+                }
+                
             }
 
             return Page();
